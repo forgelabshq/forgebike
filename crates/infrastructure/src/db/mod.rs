106ea5710f94b4e600bfdf79643e35ddd68bb549
Arthur Bowers <arthurbowers@gmail.com>
2026-07-28 15:57:32 +0100

    Adding menu_item_repository and restaurant_repository to the infrastructure.

--- crates/infrastructure/src/db/mod.rs
+++ crates/infrastructure/src/db/mod.rs
@@ -1,5 +1,9 @@
+pub mod menu_item_repository;
+pub mod restaurant_repository;
 pub mod tenant_repository;
 pub mod user_repository;
 
+pub use menu_item_repository::PgMenuItemRepository;
+pub use restaurant_repository::PgRestaurantRepository;
 pub use tenant_repository::PgTenantRepository;
 pub use user_repository::PgUserRepository;
